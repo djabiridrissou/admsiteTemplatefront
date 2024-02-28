efe76e7249c562782ae5c882af08b4089ec5d6f0
IDRISSOU Djabir <81425788+djabiridrissou@users.noreply.github.com>
2024-02-28 14:55:51 +0000

    first push - last one

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,16 @@ import './index.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {Dashboard, Layout } from "./routes/routes";
-import { useEffect } from "react";
 
 function App() {
-
-  
-  }, );
 
   return (
     <>
       <div>
         <Routes>
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="change-password" element={<ChangePassword />} /> */}
+         
           <Route path="/" element={<Layout />}>
             <Route index element={<Dashboard />} />
-           
-           {/*  <Route path="debt-list" element={<DebtList />} />
-            <Route path="users" element={<Users />} />
-            <Route path="index-users" element={<IndexUser />}>
-              <Route path="users" element={<Users />} />
-              <Route path="register" element={<Register />} />
-            </Route> */}
           </Route>
         </Routes>
         <ToastContainer />
